e54a1fe881928bedc5259c3f146d7ec4d836d6a5
Geoff Winans <gwinans@gmail.com>
2019-04-13 17:15:23 -0700

    Spacing, formatting.

--- bin/check-mysql-alive.rb
+++ bin/check-mysql-alive.rb
@@ -37,71 +37,76 @@ require 'mysql2'
 require 'inifile'
 
 class CheckMySQL < Sensu::Plugin::Check::CLI
-  option :user,
-         description: 'MySQL User',
-         short: '-u USER',
-         long: '--user USER'
+    option :user,
+        description: 'MySQL User',
+        short: '-u USER',
+        long: '--user USER'
 
-  option :password,
-         description: 'MySQL Password',
-         short: '-p PASS',
-         long: '--password PASS'
+    option :password,
+        description: 'MySQL Password',
+        short: '-p PASS',
+        long: '--password PASS'
 
-  option :ini,
-         description: 'My.cnf ini file',
-         short: '-i VALUE',
-         long: '--ini VALUE'
+    option :ini,
+        description: 'My.cnf ini file',
+        short: '-i VALUE',
+        long: '--ini VALUE'
 
-  option :ini_section,
-         description: 'Section in my.cnf ini file',
-         long: '--ini-section VALUE',
-         default: 'client'
+    option :ini_section,
+        description: 'Section in my.cnf ini file',
+        long: '--ini-section VALUE',
+        default: 'client'
 
-  option :hostname,
-         description: 'Hostname to login to',
-         short: '-h HOST',
-         long: '--hostname HOST'
+    option :hostname,
+        description: 'Hostname to login to',
+        short: '-h HOST',
+        long: '--hostname HOST'
 
-  option :database,
-         description: 'Database schema to connect to',
-         short: '-d DATABASE',
-         long: '--database DATABASE',
-         default: 'mysql'
+    option :database,
+        description: 'Database schema to connect to',
+        short: '-d DATABASE',
+        long: '--database DATABASE',
+        default: 'mysql'
 
-  option :port,
-         description: 'Port to connect to',
-         short: '-P PORT',
-         long: '--port PORT',
-         default: '3306'
+    option :port,
+        description: 'Port to connect to',
+        short: '-P PORT',
+        long: '--port PORT',
+        default: '3306'
 
-  option :socket,
-         description: 'Socket to use',
-         short: '-s SOCKET',
-         long: '--socket SOCKET'
+    option :socket,
+        description: 'Socket to use',
+        short: '-s SOCKET',
+        long: '--socket SOCKET'
 
-  def run
-    if config[:ini]
-      ini = IniFile.load(config[:ini])
-      section = ini[config[:ini_section]]
-      db_user = section['user']
-      db_pass = section['password']
-      port    = section['port'].to_i
-      socket  = section['socket']
-    else
-      db_user = config[:user]
-      db_pass = config[:password]
-      port    = config[:port].to_i
-      socket  = config[:socket]
+    def run
+        if config[:ini]
+            ini = IniFile.load(config[:ini])
+            section  = ini[config[:ini_section]]
+            hostname = section['hostname']
+            db_user  = section['user']
+            db_pass  = section['password']
+            database = section['database']
+            port     = section['port'].to_i
+            socket   = section['socket']
+        else
+            hostname = config[:hostname]
+            db_user  = config[:user]
+            db_pass  = config[:password]
+            database = config[:database]
+            port     = config[:port].to_i
+            socket   = config[:socket]
+        end
+
+        begin
+            db = Mysql2::Client.new(:hostname => config[:hostname], :username => db_user, :password => db_pass, :database =>config[:database], :port => port, :socket => socket)
+            info = db.server_info
+            ok "Server version: #{info}"
+
+            rescue Mysql2::Error => e
+                critical "Error message: #{e.error}"
+            ensure
+                db.close if db
+        end
     end
-
-    begin
-      db = Mysql2::Client.new(:hostname => config[:hostname], :username => db_user, :password => db_pass, :database =>config[:database], :port => port, :socket => socket)
-      info = db.server_info
-      ok "Server version: #{info}"
-    rescue Mysql2::Error => e
-      critical "Error message: #{e.error}"
-    ensure
-      db.close if db
-    end
-  end
 end
